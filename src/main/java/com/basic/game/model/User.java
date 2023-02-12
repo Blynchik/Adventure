81c5977c6b62817c9a657607972edc1508124260
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-02-12 09:24:25 +0300

    dependecy validation add and user entity added

---
+++ src/main/java/com/basic/game/model/User.java
@@ -0,0 +1,67 @@
+package com.basic.game.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "nick_name")
+    @Size(max = 100, message = "Должно быть меньше 100 символов")
+    private String nickName;
+
+    @Column(name = "phone_number")
+    @Size(min = 12, max = 13, message = "Должно быть 12 знаков")
+    private String phoneNumber;
+
+    @Column(name = "registered_at", nullable = false,columnDefinition = "timestamp default now()", updatable = false)
+    @NotNull
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Date registeredAt = new Date();
+
+    @Column(name = "enabled", nullable = false,columnDefinition = "bool default true")
+    @NotNull
+    private boolean enabled = true;
+
+    public User() {
+    }
+
+    public User(String nickName, String phoneNumber) {
+        this.nickName = nickName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
