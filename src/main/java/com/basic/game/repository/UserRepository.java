bcb37007e4efbbab053ee047efdbe6b2409885d1
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-02-12 09:31:58 +0300

    user repository added

---
+++ src/main/java/com/basic/game/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.basic.game.repository;
+
+import com.basic.game.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
