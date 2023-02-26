67e7771935b2c5a06a76c55d431850f14b519d02
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-02-26 13:33:38 +0300

    logger added

--- src/main/java/com/basic/game/controller/UserController.java
+++ src/main/java/com/basic/game/controller/UserController.java
@@ -4,17 +4,19 @@ import com.basic.game.dto.UserTo;
 import com.basic.game.model.User;
 import com.basic.game.service.UserService;
 import com.basic.game.util.Converter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+    public static final Logger log = LoggerFactory.getLogger(UserController.class);
 
     private final UserService userService;
 
@@ -25,6 +27,7 @@ public class UserController {
 
     @GetMapping("/{id}")
     public ResponseEntity<User> getOne(@PathVariable final int id) {
+        log.info("UserController начал getOne id:{}", id);
 
         if (userService.get(id).isPresent()) {
             return ResponseEntity.ok().body(userService.get(id).get());
@@ -35,23 +38,21 @@ public class UserController {
 
     @GetMapping
     public ResponseEntity<List<User>> getAll() {
-
-        if (!userService.getAll().isEmpty()) {
-            return ResponseEntity.ok(userService.getAll());
-        }
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Collections.emptyList());
+        log.info("UserController начал getAll");
+        return ResponseEntity.ok().body(userService.getAll());
     }
 
-    @PostMapping("/create")
+    @PostMapping()
     public ResponseEntity<User> create(@RequestBody UserTo userTo) {
+        log.info("UserController начал create");
         User user = new User();
         userService.create(Converter.getUser(user, userTo));
         return ResponseEntity.ok().body(user);
     }
 
-    @DeleteMapping("/{id}/delete")
+    @DeleteMapping("/{id}")
     public ResponseEntity<List<User>> delete(@PathVariable int id) {
+        log.info("UserController начал delete id:{}", id);
 
         if (userService.get(id).isPresent()) {
             userService.delete(id);
@@ -61,9 +62,10 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
-    @PatchMapping("/{id}/edit")
+    @PatchMapping("/{id}")
     public ResponseEntity<User> update(@PathVariable int id,
                                        @RequestBody UserTo userTo) {
+        log.info("UserController начал update id:{}", id);
 
         if (userService.get(id).isPresent()) {
             userService.update(Converter.getUser(userService.get(id).get(), userTo), id);
