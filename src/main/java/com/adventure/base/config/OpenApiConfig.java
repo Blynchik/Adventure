d90ea5141edde2fca797443d112b955b056a88a1
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-03-27 19:19:16 +0300

    simple integration with open api

---
+++ src/main/java/com/adventure/base/config/OpenApiConfig.java
@@ -0,0 +1,17 @@
+package com.adventure.base.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public GroupedOpenApi api() {
+        return GroupedOpenApi.builder()
+                .group("REST API")
+                .pathsToMatch("/game/**")
+                .build();
+    }
+}
